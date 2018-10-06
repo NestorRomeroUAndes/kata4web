8bdd6e2d34118ee0626555b4fa00bfec1bd00a31
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-10-05 20:34:43 -0500

    Commit sincronización y configuración del ambiente.

--- polls/test_cases.py
+++ polls/test_cases.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 class FunctionalTest(TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome('E:\\Nestor\\OneDrive\\Documentos\\uniandes\\201820\\procesos agiles\\chromedriver.exe')
+        #self.browser = webdriver.Chrome('E:\\Nestor\\OneDrive\\Documentos\\uniandes\\201820\\procesos agiles\\chromedriver.exe')
+        self.browser = webdriver.Chrome('E:\\Maestría MISO\\201820\\Procesos de desarrollo agiles\\chromedriver_win32\\chromedriver.exe')
         self.browser.implicitly_wait(2)
 
     def tearDown(self):
