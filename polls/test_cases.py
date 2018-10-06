2c6e2f95aae44f2c58ed5c73767f6bbfbfd90c93
nextorr <nextorr@gmail.com>
2018-10-05 20:37:38 -0500

    Paso 1. se implementa test titulo pagina (ROJO)
    Paso 1. se implementa test titulo pagina (ROJO)

--- polls/test_cases.py
+++ polls/test_cases.py
@@ -6,10 +6,13 @@ from selenium.webdriver.common.by import By
 class FunctionalTest(TestCase):
 
     def setUp(self):
-        #self.browser = webdriver.Chrome('E:\\Nestor\\OneDrive\\Documentos\\uniandes\\201820\\procesos agiles\\chromedriver.exe')
-        self.browser = webdriver.Chrome('E:\\Maestría MISO\\201820\\Procesos de desarrollo agiles\\chromedriver_win32\\chromedriver.exe')
+        self.browser = webdriver.Chrome('E:\\Nestor\\OneDrive\\Documentos\\uniandes\\201820\\procesos agiles\\chromedriver.exe')
+        #self.browser = webdriver.Chrome('E:\\Maestría MISO\\201820\\Procesos de desarrollo agiles\\chromedriver_win32\\chromedriver.exe')
         self.browser.implicitly_wait(2)
 
     def tearDown(self):
         self.browser.quit()
 
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
